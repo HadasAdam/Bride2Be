c0318decdffcf72f0361cce807e0e69adc63efa3
Hadas <hadasadam8@gmail.com>
2022-01-28 12:38:08 +0200

    Fixed issues with EditProfile, UserProfile, EditProduct, AddNewProduct

--- app/src/main/java/com/example/bride2be/MainActivity.java
+++ app/src/main/java/com/example/bride2be/MainActivity.java
@@ -13,13 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Building Fragments:
         ProductsListFragment productsListFragment = new ProductsListFragment();
         LoginFragment loginFragment = new LoginFragment();
+        AddNewProductFragment addNewProductFragment = new AddNewProductFragment();
+        EditProductFragment editProductFragment = new EditProductFragment();
+        UserProfileFragment userProfileFragment = new UserProfileFragment();
+        EditProfileFragment editProfileFragment = new EditProfileFragment();
 
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.mainactivity_fragment_container, loginFragment);
+        transaction.add(R.id.mainactivity_fragment_container, editProfileFragment);
         transaction.commit();
     }
 }
