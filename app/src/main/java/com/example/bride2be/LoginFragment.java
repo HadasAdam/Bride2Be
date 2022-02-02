7e2b186294f36f26fda9578e6470f3027ccf5104
Hadas <hadasadam8@gmail.com>
2022-02-02 17:30:19 +0200

    * added methods to buttons in login and signup fragments * created GeneralUtils for static functions * created ModelFirebase & ModelSQL to communicate with Model (Fragments should know MODEL ONLY!)

--- app/src/main/java/com/example/bride2be/LoginFragment.java
+++ app/src/main/java/com/example/bride2be/LoginFragment.java
@@ -1,13 +1,19 @@
 package com.example.bride2be;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.bride2be.models.Model;
+import com.example.bride2be.models.User;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +21,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class LoginFragment extends Fragment {
+
+    EditText emailET;
+    EditText passwordET;
+    Button submitButton;
+    Button signUpButton;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,17 +70,45 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container,false);
+        signUpButton = view.findViewById(R.id.login_frg_signup_button);
+        submitButton = view.findViewById(R.id.login_frg_submit_button);
+        emailET = view.findViewById(R.id.login_frg_email_et);
+        passwordET = view.findViewById(R.id.login_frg_password_et);
+        signUpButton.setOnClickListener(v -> onSignUpButton());
+        submitButton.setOnClickListener(v -> onClickSubmitButton());
+
+        return view;
     }
 
-    public void onClickSubmitButton(View view)
+    public void onClickSubmitButton()
     {
-        //TODO: MOVE TO USER PROFILE FRAGMENT
+        String emailAddress = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+        if (checkLoginCredentials(emailAddress, password))
+        {
+            Log.d("TAG", "User with email: " + emailAddress + " was found in database.");
+            FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainactivity_fragment_container, new UserProfileFragment());
+            fragmentTransaction.commit();
+        }
+        else
+        {
+            Log.d("TAG", "User with email: " + emailAddress +
+                    " was not found in database or put a wrong password.");
+        }
     }
 
-    public void onClickNewAccountButton(View view)
+    public void onSignUpButton()
     {
-        //TODO: MOVE TO SIGN UP FRAGMENT
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.mainactivity_fragment_container, new SignUpFragment());
+        fragmentTransaction.commit();
+    }
+
+    private boolean checkLoginCredentials(String emailAddress, String givenPassword)
+    {
+        User user = GeneralUtils.findUserByEmail(Model.instance.getAllUsers(), emailAddress);
+        return (user != null && user.getPasswordHash().equals(GeneralUtils.md5(givenPassword)));
     }
 }
---
+++ app/src/main/java/com/example/bride2be/models/ModelFirebase.java
@@ -0,0 +1,15 @@
+package com.example.bride2be.models;
+
+public class ModelFirebase {
+    public void getAllUsers(Model.GetAllUsersListener listener) {
+    }
+
+    public void addUser(User user, Model.AddUserListener listener) {
+    }
+
+    public void getAllProducts(Model.GetAllProductsListener listener) {
+    }
+
+    public void addProduct(Product product, Model.AddProductListener listener) {
+    }
+}
