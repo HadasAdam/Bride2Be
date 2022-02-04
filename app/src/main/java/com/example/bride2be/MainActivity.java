64e59537b8ac5aa73fc079a0babd246047d50b1d
liam1238 <liam1238@gmail.com>
2022-02-04 16:06:31 +0200

    add google map api to the project

--- app/src/main/java/com/example/bride2be/MainActivity.java
+++ app/src/main/java/com/example/bride2be/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.bride2be;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.math.BigDecimal;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +34,18 @@ public class MainActivity extends AppCompatActivity {
         EditProfileFragment editProfileFragment = new EditProfileFragment();
         SignUpFragment signUpFragment = new SignUpFragment();
         WishlistFragment wishlistFragment = new WishlistFragment();
+        MapsFragment mapsFragment = new MapsFragment();
 
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.mainactivity_fragment_container, loginFragment);
+        transaction.add(R.id.mainactivity_fragment_container, userProfileFragment);
         transaction.commit();
         FirebaseDatabase database = FirebaseDatabase.getInstance("https://bride2be-database-default-rtdb.europe-west1.firebasedatabase.app");
-        DatabaseReference myRef = database.getReference("users");
-        DatabaseReference usersx = myRef.child("x");
-        usersx.child("name").setValue("liam");
-        usersx.child("family").setValue("golan");
+//        DatabaseReference myRef = database.getReference("users");
+//        DatabaseReference usersx = myRef.child("x");
+//        usersx.child("name").setValue("liam");
+//        usersx.child("family").setValue("golan");
     }
+
 }
